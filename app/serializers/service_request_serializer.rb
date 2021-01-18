c4afbd7e4e8c9a1644c0c52315c4a04df637fe2d
angela stephens <angelastephens317@gmail.com>
2021-01-17 19:15:16 -0500

    add id to servicerequest serializer

--- app/serializers/service_request_serializer.rb
+++ app/serializers/service_request_serializer.rb
@@ -1,4 +1,4 @@
 class ServiceRequestSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :service_type, :pick_up_time, :pick_up_location, :dog_id
+  attributes :id, :service_type, :pick_up_time, :pick_up_location, :dog_id
 end
